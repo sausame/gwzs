eb97c8e226b42868dcbfaafcc669efb419c43e08
Song <song@china.world>
2016-05-08 18:22:49 +0800

    Support full url

--- trunk/network.py
+++ trunk/network.py
@@ -1,6 +1,16 @@
 import httplib  
 
-def saveHttpData(host, url, filename):
+def saveHttpData(filename, url, host=None):
+
+    if None == host:
+        start = url.find('//') + 2
+        end = url[start:].find('/')
+
+        host = url[start:start+end]
+        url = url[start+end:]
+
+    print host, url, filename
+
     conn = httplib.HTTPConnection(host)  
     conn.request("GET", url)
     res = conn.getresponse()
@@ -24,6 +34,9 @@ size = 5
 gids = [x for x in range(start, start + size)]
 
 for gid in gids:
-    ret = saveHttpData('coupon.m.jd.com', '/seckill/seckillList.json?gid=%d' % gid, '%d.json' % gid)
+    '''
+    ret = saveHttpData('%d.json' % gid, '/seckill/seckillList.json?gid=%d' % gid, 'coupon.m.jd.com')
+    '''
+    ret = saveHttpData('%d.json' % gid, 'http://coupon.m.jd.com/seckill/seckillList.json?gid=%d' % gid)
     print gid, ret
 
